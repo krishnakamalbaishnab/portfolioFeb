01e766f903ca8635f5f2393a06b034cfcbeee564
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-06-20 23:01:47 +0100

    Cv added public folder

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -247,7 +247,7 @@ export default function Contact() {
               </CardHeader>
               <CardContent>
                 <Button className="w-full" asChild>
-                  <a href="/resume.pdf" download>
+                  <a href="./resume.pdf" download>
                     <Download className="mr-2 h-4 w-4" />
                     Download Resume
                   </a>
